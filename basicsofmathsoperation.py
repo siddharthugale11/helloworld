83eab6bd748d96e8023631a900ee601bf5b61595
siddharthugale11 <siddharthugale45@gmail.com>
2025-07-24 15:56:01 +0530

    Create basicsofmathsoperation.py

---
+++ basicsofmathsoperation.py
@@ -0,0 +1,10 @@
+A=25
+B=15
+
+print(A+B)         #Addition
+print(A-B)         #Subtraction
+print(A*B)         #Multiplication
+print(A/B)         #Division
+print(A%B)         #Modulus
+print(A//B)        #Floor Division
+print(A**B)        #Exponentiation
