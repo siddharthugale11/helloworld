68a2aa171c397495ed8a34aeaae682663423b37e
siddharthugale11 <siddharthugale45@gmail.com>
2025-07-24 15:52:43 +0530

    Create forloop.py

---
+++ forloop.py
@@ -0,0 +1,14 @@
+for i in range(10):
+    if i == 5:
+        break
+        print(i)
+        
+for i in range(10):
+    if i == 5:
+        continue
+        print(i)
+        
+for i in range(5):
+    if i == 2:
+        pass
+        print(i)
