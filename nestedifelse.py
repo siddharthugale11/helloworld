6b66c729ee333aff67109d3c8daf0c5324282b3a
siddharthugale11 <siddharthugale45@gmail.com>
2025-07-24 15:53:58 +0530

    Create nestedifelse.py

---
+++ nestedifelse.py
@@ -0,0 +1,19 @@
+marks = float(input("Enter your marks:"))
+
+if marks >101:
+    print("Invalid Input")
+    
+elif  marks >90:
+    print("Grade A")
+
+elif  marks >75:
+    print("Grade B")
+    
+elif  marks >50:
+    print("Grade C")
+    
+elif  marks >35:
+    print("Grade D") 
+    
+else:
+    print("Fail")
